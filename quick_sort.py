e4b467b941972196ea169661a7a5ebbfdb1a8653
boikobohdan69 <147216202+boikobohdan69@users.noreply.github.com>
2023-10-25 18:34:46 -0400

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -7,13 +7,11 @@ def partition(array, low, high):
     # pointer for greater element
     i = low - 1
  
-    # traverse through all elements
-    # compare each element with pivot
+   
     for j in range(low, high):
         if array[j] <= pivot:
  
-            # If element smaller than pivot is found
-            # swap it with the greater element pointed by i
+            
             i = i + 1
  
             # Swapping element at i with element at j
@@ -21,11 +19,8 @@ def partition(array, low, high):
  
     # Swap the pivot element with the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
- 
-    # Return the position from where partition is done
     return i + 1
- 
-# function to perform quicksort
+
  
  
 def quickSort(array, low, high):
